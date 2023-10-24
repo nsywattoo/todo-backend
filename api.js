7562ed4d6fc3665a4b1896148e3c3c30a6403b7e
Nasir Wattoo <mac@192.168.10.19>
2023-10-25 01:54:39 +0500

    Initial commit

--- api.js
+++ api.js
@@ -13,6 +13,18 @@ router.get('/', async (req, res) => {
       }
 }); 
 
+// Route for display one item
+router.get('/:id', async (req, res) => {
+  try {
+      const itemId = req.params.id;
+      const todos = await Todo.findById(itemId); // Retrieve all todos from the database
+      res.status(200).json(todos); // Send the todos as a JSON response
+    //  console.log(todos)
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+}); 
+
 // Route for creating a new todo
 router.post('/', async (req, res) => {
   const { task } = req.body;
@@ -24,23 +36,42 @@ router.post('/', async (req, res) => {
   //res.redirect('/todos');
 });
 
-// Route for marking a todo as completed
-router.post('/:id/complete', async (req, res) => {
-  const { id } = req.params;
-  const todo = await Todo.findById(id);
-  todo.completed = true;
-  await todo.save();
-  res.redirect('/todos');
-});
+// // Route for marking a todo as completed
+// router.post('/:id/complete', async (req, res) => {
+//   const { id } = req.params;
+//   const todo = await Todo.findById(id);
+//   todo.completed = true;
+//   await todo.save();
+//   res.redirect('/todos');
+// });
 
+// Route to handle POST request to delete an item by ID
+router.post('/delete', async (req, res) => {
+  
+    const itemId = req.params.id; // Assuming the item ID is sent in the body of the POST request
+  
+   
+    try {
+      // Use Mongoose findByIdAndDelete with the retrieved ID
+      const deletedItem = await Todo.deleteOne(itemId);
+      if (deletedItem) {
+        res.status(200).json({ message: 'Item deleted successfully' });
+      } else {
+        res.status(404).json({ error: 'Item not found' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 // Route for deleting a todo
 
-router.delete("/todos/:id", async (req, res) => {
-  const { id } = req.params;
-  const newTodo = await Todo.findByIdAndDelete(id);
-   res.status(201).json({ message: 'deleted successfully', data: newTodo });
-})
+// router.delete("/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const newTodo = await Todo.findByIdAndDelete(id);
+//    res.status(201).json({ message: 'deleted successfully', data: newTodo });
+// })
 
 
 
